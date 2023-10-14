333ece3a602cf68bdd9a40c6ab980a956d759647
Warren R Bank <warren.r.bank@gmail.com>
2023-10-14 00:27:06 -0700

    update README and "--help" output

--- bin/dns-server/help.js
+++ bin/dns-server/help.js
@@ -17,15 +17,23 @@ options:
 "--debug"
     Print a log of each request.
 
+"-h" <filepath>
+"--hosts" <filepath>
+"--file" <filepath>
+"-j" <filepath>
+"--json" <filepath>
 "--hosts-file" <filepath>
     Specify path to input 'hosts' JSON file.
     [required]
 
+"-p" <number>
 "--port" <number>
     Specify port number for DNS server.
     [default: 53]
 
-"--fallback-server" <IP[:port]>
+"-f" <IP>
+"-s" <IP>
+"--fallback-server" <IP>
     Specify fallback DNS server
     to resolve hostnames not found in "--hosts-file".
     [default: "1.1.1.1"]
